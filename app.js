459e443a96a5ef021d2a59cf73839e93e707ef70
tjhooper1 <tomh386@gmail.com>
2018-03-12 00:53:26 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const obj = {
+    player: "bobby",
+    experience: 100,
+    wizardLevel: false
+}
+
+const player = obj.player;
+const experience = obj.experience;
+const wizardLevel = obj.wizardLevel;
+
+const{ player, experience } = obj;
