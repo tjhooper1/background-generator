a52a749a494c48a2b72464e535b1f711403843ab
tjhooper1 <tomh386@gmail.com>
2018-03-15 12:48:10 -0400

    vs code second commit

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ async function playerStart () {
     await movePlayer(10, 'Right');
     await movePlayer(330, 'Left');
 }
+
+async function playerStart() {
+    const firstMove = await movePlayer(100, 'Left');
+    await movePlayer(400, 'Left');
+    await movePlayer(10, 'Right');
+    await movePlayer(330, 'Left');
+}
